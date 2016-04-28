9202345f37e0e41e35e3d7bfc02f916077ae117a
pcawen <pablo.cawen@gmail.com>
2016-04-28 09:06:01 -0300

    The most basic working version.

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ var webpack = require("webpack");
 
 module.exports = {
   //context: __dirname + "/src",
-  entry: "./main.js",
+  entry: "./main.jsx",
   output: {
   	path: __dirname + "/dist",
   	filename: "bundle.js"
   },
   module: {
   	loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", query: {presets:['react','es2015']}}
+      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader", query: {presets:['react','es2015']}}
   	]
   },
   plugins: [
